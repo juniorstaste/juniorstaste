3fafc6ff2ae0dcd820f73b4e7839d6e20dc81e8b
Anuni <anuni@mac.speedport.ip>
2026-03-07 02:51:42 +0100

    fix near page prerender error

--- app/near/page.tsx
+++ app/near/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
+export const runtime = "edge";
 
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
